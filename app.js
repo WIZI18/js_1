40c0e010eb80e5cc62695072243ae34d5eb7b43e
WIZI18 <154735139+WIZI18@users.noreply.github.com>
2024-02-22 14:29:14 +0500

    Assignment_01

---
+++ app.js
@@ -0,0 +1,50 @@
+//Chapter 01
+// Question 01
+var greet = "Hello"
+alert(greet)
+// Question 02
+var error = "Error! Please Enter a valid password."
+alert(error)
+// Question 03
+var landing = "Wellcome to JS Land...\nHappy Coding."
+alert(landing)
+// Question 04
+var error = "Wellcome to JS Land..."
+alert(error)
+var error = "Happy Coding!"
+alert(error)
+// Question 05
+var error = "Hello... I can run JS through my web browser's console."
+alert(error)
+
+//Chapter 02
+// Question 01, 2 & 3
+
+var userName
+var userName = "MUHAMMAD ABDULLAH"
+var message = "Hello World"
+alert(message)
+//Question 04
+
+var john = "john Doe"
+var age = "15 years old"
+var p = "Certified Mobile Application Development"
+alert(john)
+alert(age)
+alert(p)
+//Question 05
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(pizza)
+//Question 06
+
+var email = "My email address is " + "wizi4856@gmail.com"
+alert(email)
+//Question 07
+
+var js = "A smarter way to learn JavaScript"
+alert(js)
+//Question 09
+
+var js = "---------&&!0!&&---------"
+alert(js)
